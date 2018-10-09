15fea900a85651805586d66ccabe06b3d74e1e76
amanda <pandafulmanda@gmail.com>
2018-10-09 07:26:45 -0500

    quick data cleaning and merging for mapping

---
+++ script.R
@@ -0,0 +1,39 @@
+library(ggmap)
+library(purrr)
+
+sites <- read.csv('./Sites-Main View.csv',
+                  stringsAsFactors = FALSE)
+
+events <- read.csv('./Events-All.csv',
+                   stringsAsFactors = FALSE)
+events <- events[c(2, 6:8, 10, 12,  14, 15, 38)]
+
+clean_address <- partial(gsub, ", \n|\n", " ")
+geocode_with_dsk <- function (address){
+  return(geocode(gsub('#[1-9][0-9]*', '', address), source = 'dsk'))
+}
+
+addresses <- unlist(lapply(sites$Address, clean_address))
+sites$Address <- addresses
+sites_lat_lons <- data.frame(Reduce(rbind, 
+                                    lapply(sites$Address, geocode_with_dsk)
+                  ))
+
+locations <- sites[c(1:6, 8)]
+locations$lat <- sites_lat_lons$lat
+locations$lon <- sites_lat_lons$lon
+
+merged <- merge(events, locations, by.x = 'Site', by.y = 'Name')
+merged <- merged[c(2, 1, 3, 4, 16, 17, 10, 6, 8, 7, 9, 5, 11:15)]
+colnames(merged)[colnames(merged) == 'Adults'] <- 'is_adults'
+colnames(merged)[colnames(merged) == 'Program.Name'] <- 'program_type'
+
+write.csv(locations, file = 'locations-cleaned.csv', row.names = FALSE)
+write.csv(events, file = 'events-cleaned.csv', row.names = FALSE)
+write.csv(merged, file = 'events-locations-merged.csv', row.names = FALSE)
+
+
+
+
+
+
